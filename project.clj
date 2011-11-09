b31efbfc0c99ff9a63bec262e8a0651152c2ef82
nex <andres@fractalmedia.mx>
2011-11-09 07:28:48 -0600

    modificaciones para mandar a github

--- project.clj
+++ project.clj
@@ -3,7 +3,4 @@
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [cj "1.0.0-SNAPSHOT"]]
-  :dev-dependencies [[org.clojars.autre/lein-vimclojure "1.0.0"]]
-            :repositories {"fractal" {:url "http://67.205.67.146:8099/archiva/repository/fractal/"}
-                           "snapshots" {:url "http://67.205.67.146:8099/archiva/repository/snapshots/"
-                                        :username "patz" :password "1qazxsw2"}})
+  :dev-dependencies [[org.clojars.autre/lein-vimclojure "1.0.0"]])
